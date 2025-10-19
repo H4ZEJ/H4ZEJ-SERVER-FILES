48a97bc9493000b3e73be0be68442a7f85e5457a
H4ZEJ <80707477+H4ZEJ@users.noreply.github.com>
2025-10-20 01:38:29 +0300

    commit

---
+++ Serverson3/libpoly/SymTable.cc
@@ -0,0 +1,12 @@
+#include "SymTable.h"
+
+using namespace std;
+
+CSymTable::CSymTable(int aTok, string aStr) : dVal(0), token(aTok), strlex(aStr)
+{
+}
+
+CSymTable::~CSymTable()
+{
+}
+//martysama0134's 8e0aa8057d3f54320e391131a48866b4
